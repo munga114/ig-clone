4ac6e82e9de75027aae483f78f9e5dafd38b9da1
munga114 <mungai.mbugua@student.moringaschool.com>
2022-06-10 02:04:17 +0300

    make changes

--- WeShare/settings.py
+++ WeShare/settings.py
@@ -161,11 +161,6 @@ USE_TZ = True
 CRISPY_TEMPLATE_PACK='bootstrap4'
 
 
-EMAIL_USE_TLS=config('EMAIL_USE_TLS', cast=bool)  
-EMAIL_HOST=config('EMAIL_HOST')  
-EMAIL_HOST_USER=config('EMAIL_HOST_USER')  
-EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')  
-EMAIL_PORT=config('EMAIL_PORT',cast=int)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
